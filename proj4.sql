e83b224ba1adb51ceedb9796ad5a0bd7039c6fa4
Anna Smith <aksmithnw@gmail.com>
2020-10-20 22:52:52 -0700

    added create table statements

---
+++ proj4.sql
@@ -0,0 +1,158 @@
+/*----------------------------------------------------------------------
+ * Name:
+ * File:
+ * Date:
+ * Class:
+ * Description:
+ ----------------------------------------------------------------------*/
+
+-- required in MariaDB to enforce constraints
+SET sql_mode = STRICT_ALL_TABLES;
+
+DROP TABLE IF EXISTS RecipesInMeals;
+DROP TABLE IF EXISTS RecipeHasDietaryRestrictions;
+DROP TABLE IF EXISTS CookingToolsRequired;
+DROP TABLE IF EXISTS IngredientOf;
+DROP TABLE IF EXISTS Instruction;
+DROP TABLE IF EXISTS Ingredient;
+DROP TABLE IF EXISTS Rating;
+DROP TABLE IF EXISTS Recipe;
+DROP TABLE IF EXISTS Meal;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS CookingTool;
+DROP TABLE IF EXISTS DietaryRestriction;
+
+CREATE TABLE DietaryRestriction(
+  restriction_id INT UNSIGNED AUTO_INCREMENT,
+  restriction_name VARCHAR(50) UNIQUE NOT NULL,
+  PRIMARY KEY (restriction_id)
+);
+
+CREATE TABLE CookingTool(
+  tool_id INT UNSIGNED AUTO_INCREMENT,
+  tool_name VARCHAR(50) UNIQUE NOT NULL,
+  PRIMARY KEY (tool_id)
+);
+
+CREATE TABLE User(
+  username VARCHAR(50) CHECK (LENGTHB(password) >= 5),
+  name VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL CHECK (LENGTHB(password) >= 8),
+  PRIMARY KEY (username)
+);
+
+CREATE TABLE Meal(
+  meal_id INT UNSIGNED AUTO_INCREMENT,
+  meal_name VARCHAR(50) NOT NULL,
+  description VARCHAR(100),
+  username VARCHAR(50),
+  PRIMARY KEY (meal_id),
+  FOREIGN KEY (username) REFERENCES User(username) ON DELETE SET NULL
+);
+
+CREATE TABLE Recipe(
+  recipe_id INT UNSIGNED AUTO_INCREMENT,
+  food_type VARCHAR(30),
+  cuisine_type VARCHAR(30),
+  recipe_title VARCHAR(30) NOT NULL,
+  username VARCHAR(50),
+  PRIMARY KEY (recipe_id),
+  FOREIGN KEY (username) REFERENCES User(username) ON DELETE SET NULL
+);
+
+CREATE TABLE Rating(
+  rating_id INT UNSIGNED AUTO_INCREMENT,
+  score SMALLINT UNSIGNED NOT NULL CHECK (score <= 5),
+  date_added DATE,
+  username VARCHAR(50),
+  recipe_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY (rating_id),
+  FOREIGN KEY (username) REFERENCES User(username) ON DELETE SET NULL,
+  FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Ingredient(
+  ingredient_id INT UNSIGNED AUTO_INCREMENT,
+  ingredient_name VARCHAR(30) UNIQUE NOT NULL,
+  recipe_id INT UNSIGNED,
+  PRIMARY KEY (ingredient_id),
+  FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id) ON DELETE SET NULL
+);
+
+CREATE TABLE Instruction(
+  recipe_id INT UNSIGNED,
+  step_number SMALLINT UNSIGNED,
+  description VARCHAR(200) NOT NULL,
+  PRIMARY KEY (recipe_id, step_number),
+  FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IngredientOf(
+  recipe_id INT UNSIGNED,
+  ingredient_id INT UNSIGNED,
+  amount DECIMAL(5, 2) UNSIGNED NOT NULL,
+  measurement_units VARCHAR(20),
+  PRIMARY KEY (recipe_id, ingredient_id),
+  FOREIGN KEY (ingredient_id) REFERENCES Ingredient(ingredient_id) ON DELETE CASCADE,
+  FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id) ON DELETE CASCADE
+);
+
+CREATE TABLE CookingToolsRequired(
+  tool_id INT UNSIGNED,
+  recipe_id INT UNSIGNED,
+  PRIMARY KEY (recipe_id, tool_id),
+  FOREIGN KEY (tool_id) REFERENCES CookingTool(tool_id) ON DELETE CASCADE,
+  FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id) ON DELETE CASCADE
+);
+
+CREATE TABLE RecipeHasDietaryRestrictions(
+  recipe_id INT UNSIGNED,
+  restriction_id INT UNSIGNED,
+  PRIMARY KEY (recipe_id, restriction_id),
+  FOREIGN KEY (restriction_id) REFERENCES DietaryRestriction(restriction_id) ON DELETE CASCADE,
+  FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id) ON DELETE CASCADE
+);
+
+CREATE TABLE RecipesInMeals(
+  recipe_id INT UNSIGNED,
+  meal_id INT UNSIGNED,
+  PRIMARY KEY (recipe_id, meal_id),
+  FOREIGN KEY (meal_id) REFERENCES Meal(meal_id) ON DELETE CASCADE,
+  FOREIGN KEY (recipe_id) REFERENCES Recipe(recipe_id) ON DELETE CASCADE
+);
+
+SHOW COLUMNS
+FROM DietaryRestriction;
+
+SHOW COLUMNS
+FROM CookingTool;
+
+SHOW COLUMNS
+FROM User;
+
+SHOW COLUMNS
+FROM Meal;
+
+SHOW COLUMNS
+FROM Recipe;
+
+SHOW COLUMNS
+FROM Rating;
+
+SHOW COLUMNS
+FROM Ingredient;
+
+SHOW COLUMNS
+FROM Instruction;
+
+SHOW COLUMNS
+FROM IngredientOf;
+
+SHOW COLUMNS
+FROM CookingToolsRequired;
+
+SHOW COLUMNS
+FROM RecipeHasDietaryRestrictions;
+
+SHOW COLUMNS
+FROM RecipesInMeals;
