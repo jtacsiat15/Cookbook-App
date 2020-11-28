8c9f87cad954fb019adb64243f070f7510938bc3
Anna Smith <>
2020-11-28 00:21:55 -0800

    added recipe search conditions

--- RecipeGUI.py
+++ RecipeGUI.py
@@ -22,7 +22,7 @@ root.title("Recipes")
 #MenuBar.pack()
 
 root.geometry("500x500")
-
+"""
 ttk.Label(root, text="Login").grid(column=3, row=1)
 
 username = StringVar()
@@ -32,7 +32,7 @@ name.grid(column = 3, row=2)
 password = StringVar()
 name = ttk.Entry(root, textvariable=password)
 name.grid(column = 3, row=3)
-
+"""
 """def login():
     loginUsername = username.get()
     loginPassword = password.get()
@@ -40,9 +40,9 @@ name.grid(column = 3, row=3)
     print(loginPassword)
     rs = con.cursor()
     getPassword = '''SELECT password
-                FROM User 
+                FROM User
                 WHERE username = "%s"'''
-    
+
     rs.execute(getPassword % (loginUsername))
     row = rs.fetchone()
     if row is not None:
@@ -62,6 +62,8 @@ name.grid(column = 3, row=3)
 
 ttk.Button(root, text="login", command=login).grid(column=3, row=4, sticky=(W,E))"""
 
+
+
 #def runApp():
 my_notebook = ttk.Notebook(root)
 my_notebook.pack()
@@ -78,7 +80,106 @@ my_notebook.add(recipeFrame, text = "Recipe Search")
 my_notebook.add(mealSearchFrame, text = "Meal Search")
 my_notebook.add(profileFrame, text = "Profile")
 
+usernameLabel = ttk.Label(recipeFrame, text="Enter usernames (separated by commas):").grid(column=1, row=1)
+usernameField = ttk.Entry(recipeFrame, width = 20)
+usernameField.grid(column=2, row = 1)
 
+IngredientLabel = ttk.Label(recipeFrame, text="Enter ingredients (separated by commas):").grid(column=1, row=2)
+IngredientField = ttk.Entry(recipeFrame, width = 20)
+IngredientField.grid(column=2, row = 2)
+
+toolLabel = ttk.Label(recipeFrame, text="Enter cooking tools (separated by commas):").grid(column=1, row=3)
+toolField = ttk.Entry(recipeFrame, width = 20)
+toolField.grid(column=2, row = 3)
+
+toolLabel = ttk.Label(recipeFrame, text="Enter cooking tools (separated by commas):").grid(column=1, row=3)
+toolField = ttk.Entry(recipeFrame, width = 20)
+toolField.grid(column=2, row = 3)
+
+dietLabel = ttk.Label(recipeFrame, text="Enter dietary restrictions (separated by commas):").grid(column=1, row=4)
+dietField = ttk.Entry(recipeFrame, width = 20)
+dietField.grid(column=2, row = 4)
+
+cuisineLabel = ttk.Label(recipeFrame, text="Enter cuisine type (separated by commas):").grid(column=1, row=5)
+cuisineField = ttk.Entry(recipeFrame, width = 20)
+cuisineField.grid(column=2, row = 5)
+
+def search():
+    query = ""
+
+    usernames = usernameField.get()
+    if(usernames != ""):
+        users = usernames.split(",")
+        usernameInput = str([u.lstrip().rstrip() for u in users])
+        usernameInput = usernameInput[1:-1]
+        q = '''SELECT r1.recipe_title, r1.recipe_id
+                FROM User u1 JOIN Recipe r1 ON (r1.username = u1.username)
+                WHERE u1.username IN ({users}) OR u1.name IN ({users})'''.format(users = usernameInput)
+        query += q
+
+    ingredients = IngredientField.get()
+    if(ingredients != ""):
+        ing_lst = ingredients.split(",")
+        input = str([i.lstrip().rstrip() for i in ing_lst])
+        input = input[1:-1]
+        q = '''SELECT DISTINCT r1.recipe_title, r1.recipe_id
+                FROM Recipe r1 JOIN IngredientOf i1 ON (r1.recipe_id = i1.recipe_id)
+                    JOIN Ingredient i2 ON (i1.ingredient_id = i2.ingredient_id)
+                WHERE i2.ingredient_name IN ({ingredients})'''.format(ingredients = input)
+        if query == "":
+            query += q
+        else :
+            query += (" INTERSECT " + q)
+
+    tools = toolField.get()
+    if(tools != ""):
+        tool_lst = tools.split(",")
+        input = str([i.lstrip().rstrip() for i in tool_lst])
+        input = input[1:-1]
+        q = '''SELECT DISTINCT r1.recipe_title, r1.recipe_id
+                FROM Recipe r1 JOIN CookingToolsRequired t1 ON (r1.recipe_id = t1.recipe_id)
+                    JOIN CookingTool t2 ON (t1.tool_id = t2.tool_id)
+                WHERE t2.tool_name IN ({tools})'''.format(tools = input)
+        if query == "":
+            query += q
+        else :
+            query += (" INTERSECT " + q)
+
+    restrictions = dietField.get()
+    if(restrictions != ""):
+        rst_lst = restrictions.split(",")
+        input = str([i.lstrip().rstrip() for i in rst_lst])
+        input = input[1:-1]
+        q = '''SELECT DISTINCT r1.recipe_title, r1.recipe_id
+                FROM Recipe r1 JOIN RecipeHasDietaryRestrictions d1 ON (r1.recipe_id = d1.recipe_id)
+                    JOIN DietaryRestriction d2 ON (d1.restriction_id = d2.restriction_id)
+                WHERE d2.restriction_name IN ({tools})'''.format(tools = input)
+        if query == "":
+            query += q
+        else :
+            query += (" INTERSECT " + q)
+
+    cuisines = cuisineField.get()
+    if(cuisines != ""):
+        c_list = cuisines.split(",")
+        input = str([c.lstrip().rstrip() for c in c_list])
+        input = input[1:-1]
+        q = '''SELECT recipe_title, recipe_id
+                FROM Recipe
+                WHERE cuisine_type IN ({cuisines})'''.format(cuisines = input)
+        if query == "":
+            query += q
+        else :
+            query += (" INTERSECT " + q)
+
+    rs = con.cursor()
+    rs.execute(query)
+
+    for (title, id) in rs:
+        print(title, ", ", id)
+
+
+searchButton = ttk.Button(recipeFrame, text="Search", command=search).grid(column=2, row=6)
 
 
 #login = StringVar()
@@ -93,7 +194,7 @@ def login():
     print(loginUsername)
     rs = con.cursor()
     query = '''SELECT COUNT(*)
-                FROM User 
+                FROM User
                 WHERE username = "%s"'''
     rs.execute(query % (loginUsername))
 
