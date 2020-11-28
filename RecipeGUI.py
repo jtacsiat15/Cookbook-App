a882d91d4d8f513418787e3b48129931777b2247
Jalen Tacsiat <jtacsiat@zagmail.gonzaga.edu>
2020-11-27 20:50:33 -0800

    added first query

--- RecipeGUI.py
+++ RecipeGUI.py
@@ -26,49 +26,39 @@ root.geometry("500x500")
 my_notebook = ttk.Notebook(root)
 my_notebook.pack()
 
-my_frame1 = Frame(my_notebook, width= 500, height = 500, bg = "blue")
-my_frame2 = Frame(my_notebook, width= 500, height = 500, bg = "red")
+recipeFrame = Frame(my_notebook, width= 500, height = 500)
+mealSearchFrame = Frame(my_notebook, width= 500, height = 500)
+profileFrame = Frame(my_notebook, width= 500, height = 500)
 
-my_frame1.pack(fill = "both", expand = 1)
-my_frame2.pack(fill = "both", expand = 1)
+recipeFrame.pack(fill = "both", expand = 1)
+mealSearchFrame.pack(fill = "both", expand = 1)
+profileFrame.pack(fill = "both", expand = 1)
 
-my_notebook.add(my_frame1, text = "Recipe Search")
-my_notebook.add(my_frame2, text = "Meal Search")
-#tab1 = ttk.Frame(MenuBar)
-#MenuBar.add(tab1, text='Recipe Search')
-#tab2 = ttk.Frame(MenuBar)
-#MenuBar.add(tab1, text='Meal Search')
+my_notebook.add(recipeFrame, text = "Recipe Search")
+my_notebook.add(mealSearchFrame, text = "Meal Search")
+my_notebook.add(profileFrame, text = "Profile")
 
-mainframe = ttk.Frame(root, padding="5 5 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+
 
 
 #login = StringVar()
-ttk.Label(mainframe, text="login").grid(column=3, row=1)
+ttk.Label(profileFrame, text="Login").grid(column=3, row=1)
 
 username = StringVar()
-name = ttk.Entry(mainframe, textvariable=username)
+name = ttk.Entry(profileFrame, textvariable=username)
 name.grid(column = 3, row=2)
-
-#loginUsername = username.get()
-#rs = con.cursor()
-#query = '''SELECT COUNT(*)
-#            FROM user 
-#            WHERE username = %s'''
-#rs.execute(query, (loginUsername))
-
 
 def login():
     loginUsername = username.get()
     print(loginUsername)
     rs = con.cursor()
     query = '''SELECT COUNT(*)
-                FROM user 
-                WHERE username = %s'''
-    rs.execute(query, (loginUsername)) 
+                FROM User 
+                WHERE username = "%s"'''
+    rs.execute(query % (loginUsername))
 
-ttk.Button(mainframe, text="login", command=login).grid(column=3, row=3, sticky=(W,E))
+ttk.Button(profileFrame, text="login", command=login).grid(column=3, row=3, sticky=(W,E))
+
+
 
 root.mainloop()
